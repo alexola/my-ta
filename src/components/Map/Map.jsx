38b2e60978f6d759582a2c07427c2a13e447dc30
alexo <alexola.22@gmail.com>
2022-05-02 16:38:38 +0100

    Map Api connected with styles

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography , useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab';
+
+import useStyles from './styles';
 
 const Map = () => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(min-width:600px)');
+const coordinates = { lat:0, lng: 0};
+
   return (
-    <div>Map</div>
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKey={{ key: 'AIzaSyCbrJwvrJ6aTTfh0msvLDNP0v7hMfHFyzw' }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50,50,50,50]}
+        >
+      </GoogleMapReact>
+    </div>
   );
 }
 
