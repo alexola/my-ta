a0623a02091dc964ac6db5afe4ae86c936471029
alexo <alexola.22@gmail.com>
2022-05-03 16:48:08 +0100

    Map worked but got key wrong

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,25 @@
 import axios from 'axios';
 
-const getPlacesData = async () => {
-  try {
-      const response = await axios.get();
-  } catch (error){
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
 
+
+export const getPlacesData = async (sw, ne) => {
+  try {
+      const { data : { data } } = await axios.get(URL, {
+        params: {
+          bl_latitude: sw.lat,
+          tr_latitude: ne.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+        },
+        headers: {
+          'X-RapidAPI-Host': '',
+          'X-RapidAPI-Key': ''
+        }
+      });
+
+      return data;
+  } catch (error){
+    console.log(error)
   }
 }
